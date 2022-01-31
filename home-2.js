dcd3eeb74f03462a8f9d50128215da9540dc8da4
Yalof <olyastaklin@gmail.com>
2022-02-01 00:12:59 +0300

    changed files

--- home-2.js
+++ home-2.js
@@ -1,65 +1,57 @@
 'use strict';
 
+const validation = (fieldName) => {
+    let value;
+    if (fieldName === 'age') {
+        value = +prompt(`Enter your ${fieldName}`);
 
-let surName = prompt('Ваша фамилия:');
-while (surName === '') {
-    alert(`Вы не ввели фамилию. Попробуйте еще раз:`);   //как иначе выводить ошибку?кроме alert
-    surName = prompt('Ваша фамилия:');
-};
-let yourName = prompt('Ваше имя:');
-while (yourName === '') {
-    alert(`Вы не ввели имя. Попробуйте еще раз:`);
-    yourName = prompt('Введите Ваше имя:');
-};
-
-let fatherName = prompt('Ваше отчество:');
-while (fatherName === '') {
-    alert(`Вы не ввели отчество. Попробуйте еще раз:`);
-    fatherName = prompt('Ваше отчество:');
-};
-
-
-const fullName = `${surName} ${yourName} ${fatherName}`;
-
-let age = +prompt('Ваш возраст в годах:');
-while (isNaN(age) || age < 1) {
-    alert(`Попробуйте еще раз:`);
-    age = +prompt('Ваш возраст:', '');
-};
-const ageYears = age;
-const ageDays = age * 365;
-const ageFiveYears = age + 5;
-let retirement = null;
-
-
-const youMale = confirm('Ваш пол - мужской?');
-const male = youMale ? 'мужcкой' : 'женский';
-if ((youMale && age >= 65) || (!youMale && age >= 55)) {
-    retirement = 'да';
-} else {
-    retirement = 'нет';
-};
-
-
-/*function valid (valueForAge){ 
-    if (valueForAge === Number) {
-        return true;
+        while (!value || !Number.isInteger(value)) {
+            value = +prompt(`Enter your ${fieldName} again`);
+        }
     } else {
-        return false
-    }*/
+        value = prompt(`Enter your ${fieldName}`);
 
-alert(`
-    Ваше Ф.И.О.: ${fullName} \n
-    Ваш возраст в годах: ${ageYears} \n
-    Ваш возраст в днях: ${ageDays} \n
-    Через 5 лет вам будет: ${ageFiveYears} \n
-    Ваш пол: ${male} \n
-    Вы на пенсии: ${retirement}
-`);
+        while (!value) {
+            value = prompt(`Enter your ${fieldName} again`)
+        }
+    }
+    return value;
+};
+
+const getGender = () => {
+    return confirm("Are you man") ? 'man' : 'woman';
+
+}
+const getPension = (gender, age) => {
+    if ((gender === 'man' && age > 64) || (gender === 'woman' && age > 54)) {
+        return "yes";
+    } else {
+        return "no";
+    }
+};
 
 
 
+const showInfo = (username, surname, patr, age, gender, pension) => {
+    alert(`ФИО: ${username} ${surname} ${patr}
+    Ваш возраст в годах: ${age}
+    Ваш возраст в днях: ${age * 365}
+    Через 5 лет Вам будет: ${age + 5}
+    Ваш пол: ${gender}
+    Вы на пенсии: ${pension}`);
+};
+
+const init = () => {
+    const username = validation('username');
+    const surname = validation('surname');
+    const patr = validation('patr');
+    const age = validation('age');
+    const gender = getGender();
+    const pension = getPension(gender, age);
 
 
+    showInfo(username, surname, patr, age, gender, pension);
+};
 
+init();
 
---
+++ todo.js
@@ -0,0 +1,122 @@
+"use strict";
+const form = document.querySelector("#form");
+const todo = document.querySelector("#todo");
+const inputText = document.querySelector("#inputText");
+const btnSubmit = document.querySelector("#btnSubmit");
+const btnClearAll = document.createElement("button");
+const modal = document.createElement("div");
+
+const btnEdit = document.createElement("button");
+const btnClose = document.createElement("button");
+
+let data = [];
+
+btnSubmit.addEventListener("click", (event) => {
+  event.preventDefault(); //перезагрузка отменяется. отменяет действие браузера по умолчанию
+
+  data.push(inputText.value); // const inputValue был ниже заменен на
+
+  todo.innerHTML = ""; //очищение содержимого блока чтобы не дублировались новые задачи по неск раз
+
+  data.forEach((title, index) => {
+    todo.innerHTML += `                          
+    <div class='card' id=${index}> 
+    ${title} 
+    <div class='btnCard'> <button id='btnDelete'> <strong> Delete </strong> </button>
+    <button id='btnDone'> <strong> Done </strong> </button>
+    </div>
+    </div>
+    `;
+  });
+
+  // + чтобы добавлялись значения вниз, а не переписывались заново
+
+  form.reset(); //очищает инпут
+});
+
+//событие на весь блок повесили ниже
+todo.addEventListener("click", (event) => {
+  //ниже для проверки чтобы только для кнопки работало
+  if (event.target.id === "btnDelete") {
+    const card = event.target.closest(".card"); //находит род.элемент,ближ. с классом кард
+    const cardId = +card.id; //ищем по айди элемент
+    data.splice(cardId, 1);
+
+    todo.innerHTML = "";
+
+    data.forEach((title, index) => {
+      todo.innerHTML += `                          
+    <div class='card' id=${index}> 
+    ${title} 
+    <div class='btnCard'> <button id='btnDelete'> <strong> Delete </strong> </button>
+    <button id='btnDone'> <strong> Done </strong> </button>
+    </div>
+    </div>
+    `;
+    });
+    console.log(card);
+  }
+  //console.log('TARGET', event.target) ;  //на чем обрабатывает
+
+  //console.log('CURENT_TARGET', event.currentTarget) ; //вешаем событие  на зел блок (id todo)
+});
+
+/// Создаем событие для кнопки Done
+todo.addEventListener("click", (event) => {
+  if (event.target.id === "btnDone") {
+    const card = event.target.closest(".card");
+    const cardId = +card.id;
+    card.classList.add("activeCard");
+
+    todo.innerHTML = "";
+    data.forEach((title, index) => {
+      todo.innerHTML += `                          
+    <div class='card' id=${index}> 
+    ${title} 
+    <div class='btnCard'> <button id='btnDelete'> <strong> Delete </strong> </button>
+    <button id='btnDone'> <strong> Done </strong> </button>
+    </div>
+    </div>
+    `;
+    });
+  }
+});
+
+// ниже создаем кнопку Clear All
+form.appendChild(btnClearAll);
+btnClearAll.className = "btnClearAll";
+btnClearAll.textContent = "CLEAR";
+
+todo.addEventListener("click", (event) => {
+  //ниже для проверки чтобы только для кнопки работало
+  if (event.target.className === "btnClearAll") {
+    const card = event.target.closest(".card");
+    const cardId = +card.id;
+    data.splice(cardId);
+
+    todo.innerHTML = "";
+  }
+});
+
+// создаем кнопку и модальное окно
+form.append(btnEdit);
+btnEdit.className = "btnEdit";
+btnEdit.textContent = "Edit";
+
+form.append(btnClose);
+btnClose.className = "btnClose";
+btnClose.textContent = "Close";
+
+form.append(modal);
+modal.className = "modalClass";
+modal.textContent = "This is a modal window";
+
+//открывается окно при клике
+btnEdit.addEventListener("click", (ev) => {
+  ev.preventDefault();
+  modal.style.display = "block";
+});
+// закрытие при клике на кнопку
+btnClose.addEventListener("click", (ev) => {
+  modal.style.display = "none";
+});
